ccbda5d66a27b0ae7fa0af9a786a5596ac31542f
Lang <73976981+GilangSan@users.noreply.github.com>
2025-03-02 21:21:49 +0700

    Create redirectDetective.js

---
+++ redirectDetective.js
@@ -0,0 +1,31 @@
+/**
+ * Redirect Detective Scraper
+ * @author Lang 
+ * @package axios, cheerio
+ * @function redirectDetective(url)
+ * dont delete wm kyah
+ */
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function redirectDetective(url) {
+    let formData = new FormData()
+    formData.append('w', url)
+    let { data } = await axios.post('https://redirectdetective.com/ld.px', formData, {
+    formData.append('f', false)
+        headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "origin": "https://redirectdetective.com",
+            "referer": "https://redirectdetective.com/",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+        }
+    })
+    const $ = cheerio.load(data)
+    let redirectTo = $('.tooltips').last().text().trim()
+
+    return {
+        originalUrl: url,
+        redirectTo
+    }
+}

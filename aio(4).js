e4a0859de77effbd63d35035acdc4fcc603f1610
Lang <73976981+GilangSan@users.noreply.github.com>
2025-08-23 18:22:30 +0700

    Add files via upload

---
+++ aio(4).js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+async function aio(url) {
+    if (!url) throw new Error('No URL provided');
+    try {
+        let { data } = await axios.post('https://tera.backend.live/allinone', { url }, {
+            headers: {
+                Host: 'tera.backend.live',
+                'User-Agent': 'okhttp/5.0.0-alpha.10',
+                'x-api-key': 'pxrAEVHPV2S0yczPyv9bE9n8JryVwJAw'
+            }
+        })
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+(async () => {
+    let result = await aio('https://www.instagram.com/p/DNpmAqzPwd7/?utm_source=ig_web_copy_link');
+    console.log(result);
+})()
